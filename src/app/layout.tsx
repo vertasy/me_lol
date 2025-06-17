bc54c3608dbaea5d68d7c7bf614764d32d1d56c1
Kaniel Outis <vnun.b2b@gmail.com>
2025-06-17 19:13:30 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Head from "next/head";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -13,6 +14,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-9XS7S8ZZMC"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-9XS7S8ZZMC');
+        </script>
+      </Head>
       <body className={``}>{children}</body>
     </html>
   );
